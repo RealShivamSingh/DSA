543ca8aec87fa65277c8b44d44e1471d2d969846
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2023-07-04 11:27:06 +0530

    Find the average of 3 given numbers.

---
+++ src/conditional_statement_Q2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class conditional_statement_Q2 {
+    //Find the average of 3 given numbers.
+    public static void main(String[] args) {
+        int d;
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+        d=(a+b+c)/3;
+        System.out.println(d);
+
+    }
+}

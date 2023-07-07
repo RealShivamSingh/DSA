eae2fa5d204e77577296070a95f2d7cf64f3c4b3
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2023-07-07 23:33:09 +0530

    Update Pattern_rectangular_Q4.java

--- src/Pattern_rectangular_Q4.java
+++ src/Pattern_rectangular_Q4.java
@@ -3,7 +3,7 @@
           ****** */
 import java.util.Scanner;
 
-public class Pattern_rectangular_Q3 {
+public class Pattern_rectangular_Q4 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int r= sc.nextInt();

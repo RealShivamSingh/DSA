7fc5867a923c0ac9f2275e18aa8db2839957d467
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2023-07-08 13:16:12 +0530

    Pattern for Print Hollow Rectangle Star

--- src/Pattern_Q3.java
+++ src/Pattern_Q3.java
@@ -5,7 +5,7 @@
         54321 */
 import java.util.Scanner;
 
-public class Pattern_Q4 {
+public class Pattern_Q3 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int r= sc.nextInt();
---
+++ src/Pattern_Q4.java
@@ -0,0 +1,24 @@
+/*      *****
+        *   *
+        *   *
+        *   *
+        *****     */
+import java.util.Scanner;
+public class Pattern_Q4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int r= sc.nextInt();
+        int cl= sc.nextInt();
+        for (int i=1;i<=r;i++){
+            for (int j=1;j<=cl;j++){
+                if (i==1|| i==r || j==1 || j==cl) {
+                    System.out.print("*");
+                }
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+            }
+        }
+    }
+

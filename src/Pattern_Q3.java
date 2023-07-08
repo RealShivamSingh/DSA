65effb1617c7747b4b89a58407f51f21dc1a2d9e
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2023-07-08 13:13:48 +0530

    Update Pattern_Q3.java

--- src/Pattern_Q3.java
+++ src/Pattern_Q3.java
@@ -5,13 +5,12 @@
         54321 */
 import java.util.Scanner;
 
-public class Pattern_Q3 {
+public class Pattern_Q4 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int r= sc.nextInt();
-        int cl= sc.nextInt();
         for (int i=1;i<=r;i++){
-            for (int j=cl;j>=1;j--){
+            for (int j=r;j>=1;j--){
                 System.out.print(j);
             }
             System.out.println();

20f228bbe2fff3292ea1e1da1afb754d4bea35a5
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2023-07-11 11:45:10 +0530

    Numerical Rectangular Pattern

---
+++ src/Numerical_Rectanglar_Pattern_Q12.java
@@ -0,0 +1,22 @@
+/*      123456
+        234561
+        345612
+        456123
+        561234
+        612345       */
+import java.util.Scanner;
+public class Numerical_Rectanglar_Pattern_Q12 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        for (int i=1;i<=r;i++){
+            for (int j=i;j<=r;j++){
+                System.out.print(j);
+            }
+            for (int k=1;k<=i-1;k++){
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+}

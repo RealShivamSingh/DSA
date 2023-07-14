46cd232baec7eb6e5fd3ca6ee9732113a159746d
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2023-07-14 13:08:37 +0530

    OOPs Basic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+class student{
+    int rollNumber=786;
+    String subject="Math";
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        student obj1=new student();
+        System.out.println(obj1.rollNumber);
+        System.out.print(obj1.subject);
+
     }
+
 }

40ca9f8cab7acd6dad63e5f14b398f6d38f05d9f
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2023-07-19 11:32:52 +0530

    Array Question 1 basic

---
+++ src/Array_Q1.java
@@ -0,0 +1,18 @@
+class arrayExample{
+    void demoArray(){
+        int [] ages=new int[3];
+        ages[0]=54;
+        ages[1]=89;
+        ages[2]=52;
+        System.out.println(ages[0]);
+        System.out.println(ages[1]);
+        System.out.println(ages[2]);
+    }
+}
+public class Array_Q1 {
+    public static void main(String[] args) {
+        arrayExample obj1=new arrayExample();
+        obj1.demoArray();
+
+    }
+}

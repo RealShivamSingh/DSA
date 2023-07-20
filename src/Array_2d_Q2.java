85597efda35c28f50053afe6a321db5951f282e9
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2023-07-20 20:01:41 +0530

    2D Arrray Demo Question

---
+++ src/Array_2d_Q2.java
@@ -0,0 +1,24 @@
+class arrayQuestion{
+    void demoArray(){
+        int[][] demo={{56,48,89},{78,45,12},{56,47,23}};
+        System.out.println(demo[0][1]);
+        System.out.println(demo[1][1]);
+        System.out.println(demo[0][2]);
+        System.out.println(demo[1][2]);
+      /*  int [][] demo=new int [5][7];
+        demo[0][1]=89;
+        demo[0][2]=74;
+        demo[1][1]=89;
+        System.out.println(demo[0][1]);
+        System.out.println(demo[1][1]);
+        System.out.println(demo[0][2]);*/
+
+    }
+}
+public class Array_2d_Q2 {
+    public static void main(String[] args) {
+        arrayExample obj =new arrayExample();
+        obj.demoArray();
+
+    }
+}

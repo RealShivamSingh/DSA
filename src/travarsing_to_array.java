f1fbb01f8783a1cb39a6b1307205d267576a6afe
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2023-07-22 22:38:07 +0530

    Traversing to Array Basic

---
+++ src/travarsing_to_array.java
@@ -0,0 +1,14 @@
+class arrayExamples{
+    void traversingDemo(){
+        int [] age={4,5,6};
+        for (int i=0;i<3;i++){
+            System.out.println(age[i]);
+        }
+    }
+}
+public class travarsing_to_array {
+    public static void main(String[] args) {
+        arrayExamples obj=new arrayExamples();
+        obj.traversingDemo();
+    }
+}

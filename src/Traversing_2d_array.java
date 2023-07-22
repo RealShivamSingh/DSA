c949d230a73b16e1aadccf745a5ab37a276cee35
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2023-07-22 22:45:30 +0530

    Traversing to 2D Array Basic

---
+++ src/Traversing_2d_array.java
@@ -0,0 +1,18 @@
+class exmaple{
+    void age(){
+        int [][]arr ={{8,9,2},{9,6,7}};
+        for (int i=0;i<2;i++){
+            for (int j=0;j<3;j++){
+                System.out.println(arr[i][j]);
+            }
+        }
+    }
+
+        }
+public class Traversing_2d_array {
+    public static void main(String[] args) {
+        exmaple obj=new exmaple();
+        obj.age();
+
+    }
+}

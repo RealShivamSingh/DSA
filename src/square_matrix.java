1ee3a65720700694de722c8bbf77570070a8f79d
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2023-08-27 15:46:46 +0530

    Square Matrix

---
+++ src/square_matrix.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class square_matrix {
+    static void printArray(int [][] matrix){
+        for (int i=0;i<matrix.length;i++){
+            for (int j=0;j<matrix[i].length;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println(" ");
+        }
+    }
+    static void transposeMatrix(int [][] matrix,int r,int c){
+        for (int i=0;i<r;i++){
+            for (int j=i;j<c;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+
+    }
+    static void reverseArray(int[]arr){
+        int i=0;int j=arr.length-1;
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+    static void rotateMatrix(int [][] matrix,int n){
+        transposeMatrix(matrix,n,n);
+        for (int i=0;i<n;i++){
+           reverseArray(matrix[i]);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size of row");
+        int r=sc.nextInt();
+        System.out.println("Enter size of column");
+        int c= sc.nextInt();
+        int [][] matrix=new int[r][c];
+        int totalElement=c*r;
+        for (int i=0;i<r;i++){
+            for (int j=0;j<c;j++){
+                matrix[i][j]= sc.nextInt();
+            }
+        }
+        System.out.println("Input Matrix");
+       printArray(matrix);
+        rotateMatrix(matrix,r);
+        System.out.println("Rotate Matrix is");
+        printArray(matrix);
+    }
+
+}

9e1791c38e4a79a9519497f7130b5a6b386f15c6
Shivam Singh <121084486+RealShivamSingh@users.noreply.github.com>
2023-09-29 21:55:08 +0530

    sorting array

--- src/sorting_array.java
+++ src/sorting_array.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class sorting_array {
     public static void main(String[] args) {
-        int [] arr={7,5,2,9,1};
+        int [] arr={-7,5,2,9,-1};
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
 
